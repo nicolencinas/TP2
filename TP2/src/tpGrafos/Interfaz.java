8c2289292b91bbf67171829c58c088f05e52da4d
nickito <nickito@192.168.0.17>
2018-09-30 21:30:32 -0300

    Cambios importantes en la interfaz grafica

--- TP2/src/tpGrafos/Interfaz.java
+++ TP2/src/tpGrafos/Interfaz.java
@@ -1,17 +1,26 @@
 package tpGrafos;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Image;
 import java.awt.Point;
 
 import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.ToolTipManager;
 import javax.swing.UIManager;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
+
+import Animacion.Animacion;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -28,8 +37,10 @@ public class Interfaz
 	LinkedList <JLabel> select=new LinkedList<JLabel>();
 	ArrayList <Integer> relaciones=new ArrayList <Integer>();
 	ArrayList <JLabel> nodos=new ArrayList <JLabel>();
-	private Integer ub=2;
+	private Integer ub=0;
 	JLabel rel=new JLabel(0+" A "+0);
+	Color color=new Color (151, 15, 207  );
+			
 	
 	
 	
@@ -67,6 +78,7 @@ public class Interfaz
 	{
 	 
 		JLabel label=new JLabel(id+"");
+		
 		label.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseReleased(MouseEvent e) 
@@ -81,11 +93,11 @@ public class Interfaz
 		});
 	 
 	
-		label.setSize(30, 30);
+		label.setSize(70, 70);
 		label.setLocation(p);
 
 		Image im=new ImageIcon(iconos[id]).getImage();
-		label.setIcon(new ImageIcon( im.getScaledInstance(30, 30, Image.SCALE_SMOOTH)));
+		label.setIcon(new ImageIcon( im.getScaledInstance(70, 70, Image.SCALE_SMOOTH)));
 		label.setText(id+"");
 		
 		return label;
@@ -96,31 +108,12 @@ public class Interfaz
 	
 	public JLabel generarNodo(int id,int ub, Point p) 
 	{
-	 
-		JLabel label=new JLabel(ub+"")
-		{
-		      /**
-			 * 
-			 */
-			private static final long serialVersionUID = 1L;
-
-			public Point getToolTipLocation(MouseEvent event)
-		      {
-				
-		        return new Point(7, 7);
-		        
-		      }
-			
-			
-		    };
-		          
+		
+		JLabel label=new JLabel(ub+"");
 	
-		ToolTipManager.sharedInstance().setInitialDelay(0);
-		ToolTipManager.sharedInstance().setDismissDelay(Integer.MAX_VALUE);
 		
-		
-		label.setToolTipText(ub+"");
-		label.addMouseListener(new MouseAdapter() {
+		label.addMouseListener(new MouseAdapter() 
+		{
 			@Override
 			public void mouseReleased(MouseEvent e) 
 			{
@@ -136,26 +129,40 @@ public class Interfaz
 				else 
 				{
 				relaciones.add(num);
-				nodos.add(label);
+				//nodos.add(label);
 				}
 				
 				
 				if (relaciones.size()==1)
 					cambiarLabel(rel,relaciones.get(0),0);
-				 if (relaciones.size()==2)
-					cambiarLabel(rel,relaciones.get(0),relaciones.get(1));
+				
+				 if (relaciones.size()==2) 
+				 {
+					 cambiarLabel(rel,relaciones.get(0),relaciones.get(1));
+				 
+				 JOptionPane.showInputDialog("Crear arista entre: "+nodos.get(relaciones.get(0)).getText()+ " y "+nodos.get(relaciones.get(1)).getText());
+				
+				 }
+					
 				
 			}
 
 			
 		});
-	 
-	
+		
 		label.setSize(30, 30);
 		label.setLocation(p);
+		
 
 		Image im=new ImageIcon(iconos[id]).getImage();
 		label.setIcon(new ImageIcon( im.getScaledInstance(30, 30, Image.SCALE_SMOOTH)));
+		JLabel numero=new JLabel(ub+"");
+		numero.setFont(new Font ("Tahoma", Font.PLAIN, 16));
+	
+		label.setLayout(new FlowLayout(FlowLayout.CENTER));
+		label.add(numero);
+		label.repaint();
+		numero.repaint();
 		
 		return label;
 		
@@ -170,6 +177,19 @@ public class Interfaz
 		label.setText(desde+" A "+ hasta);
 		
 	}
+	
+	//Utilizada como recurso grafico para cambiar imagen de un drop menu
+	public void im_up(JLabel icono)
+	{
+icono.setIcon(new ImageIcon("subir.png"));
+	}
+	
+	//Utilizada como recurso grafico para cambiar imagen de un drop menu
+	public void im_down(JLabel icono)
+	{
+icono.setIcon(new ImageIcon("bajar.png"));
+	}
+
 	private void initialize() 
 	{
 		frame = new JFrame();
@@ -178,29 +198,114 @@ public class Interfaz
 		frame.getContentPane().setLayout(null);
 		
 		frame.setFocusable(true);
-		rel.setLocation(1500, 0);
+		rel.setLocation(400, 0);
 		rel.setFont(new Font("Tahoma", Font.PLAIN, 26));
 		rel.setSize(100,100);
 		rel.setVisible(true);
 		frame.getContentPane().add(rel);
 	
 
+		JPanel dropmenu= new JPanel();
+		dropmenu.setLayout(null);
 		
-		Point p1=new Point(71, 36);
-		Point p2=new Point(586, 36);
-		Point p3=new Point(977, 36);
+		dropmenu.setLocation(0,0);
+		dropmenu.setBorder( new RoundedBorder(10,color));
+		dropmenu.setSize(350,130);
+		
+		JLabel icono = new JLabel("");
+		icono.setBounds(165, 114, 30, 15);
+		icono.setBackground(Color.WHITE);
+		icono.setIcon(new ImageIcon("subir.png"));
+		icono.setToolTipText("Desplegar drop menu");
+		
+		
+		dropmenu.add(icono);
+		
+		frame.add(dropmenu);
+		
+		Point p1=new Point(10, 36);
+		Point p11=new Point(10,18);
+		Point p2=new Point(130, 36);
+		Point p22=new Point (03,18);
+		Point p3=new Point(250, 36);
+		Point p33=new Point (22,18);
 		
 		
 		JLabel productor = generarIcono(0, p1);
+		
 		JLabel consumidor = generarIcono(1, p2);
+	
 		JLabel paso = generarIcono(2, p3);
 		
-		frame.add(paso);
-		frame.add(consumidor);
-		frame.add(productor);
+		
+		productor.setLayout(null);
+		consumidor.setLayout(null);
+		paso.setLayout(null);
+		
+		
+		JLabel productorTitle = new JLabel("Productor");
+		productorTitle.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		productorTitle.setSize(100,30);
+		productorTitle.setLocation(p11);
+		
+		productor.add(productorTitle);
+		
+		JLabel consumidorTitle = new JLabel("Consumidor");
+		consumidorTitle.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		consumidorTitle.setSize(100,30);
+		consumidorTitle.setLocation(p22);
+		consumidor.add(consumidorTitle);
+		
+		JLabel pasoTitle = new JLabel("Paso");
+		pasoTitle.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		pasoTitle.setSize(100,30);
+		pasoTitle.setLocation(p33);
+		paso.add(pasoTitle);
+		
+		dropmenu.add(paso);
+		dropmenu.add(consumidor);
+		dropmenu.add(productor);
+		
+	
+		
+		//JLabel consumidor = generarIcono(1, p2);
+		//JLabel paso = generarIcono(2, p3);
+		
+		
 		select.add(paso);
 		select.add(consumidor);
 		select.add(productor);
+		
+		
+		icono.addMouseListener(new MouseAdapter() 
+		{
+
+			public void mouseReleased(MouseEvent e)
+			{
+		int posicion=dropmenu.getY();
+		if (posicion>=0)
+		{
+			
+			
+			Animacion.subir(0, -115, 2,1,dropmenu);
+			dropmenu.setBorder(null);
+			icono.setToolTipText("Desplegar drop menu");
+			im_down(icono);
+		}else
+		{
+			
+			Animacion.bajar(-100, 0, 2,1, dropmenu);
+			dropmenu.setBorder(new RoundedBorder(10, color));
+			im_up(icono);
+			icono.setToolTipText("Retraer drop menu");
+			
+		}
+
+	}
+		});
+		
+		
+		
 		
 			frame.addMouseListener(new MouseAdapter() 
 		{
@@ -223,8 +328,15 @@ public class Interfaz
 				{
 				JLabel l=generarNodo(i,ub,p);
 				frame.add(l);
+				nodos.add(l);
 				frame.repaint();
 					ub++;
+					
+					for (JLabel lab:nodos) 
+					{
+						System.out.print(lab.getText()+" ");
+					}
+					System.out.println("");
 				}
 				
 				
---
+++ TP2/src/tpGrafos/RoundedBorder.java
@@ -0,0 +1,38 @@
+package tpGrafos;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Insets;
+import javax.swing.border.Border;
+
+class RoundedBorder implements Border
+{
+
+    private int radius;
+    private Color color;
+
+    RoundedBorder(int radius, Color color)
+    {
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public Insets getBorderInsets(Component c) {
+        return new Insets(this.radius+1, this.radius+1, this.radius+2, this.radius);
+    }
+
+    public boolean isBorderOpaque() {
+        return true;
+    }
+    
+    
+
+    public void paintBorder(Component c, Graphics g, int x, int y, int width, int height)
+    {
+    	
+    	g.setColor(color);
+    	
+        g.drawRoundRect(x, y, width-1, height-1, radius, radius);
+    }
+}
